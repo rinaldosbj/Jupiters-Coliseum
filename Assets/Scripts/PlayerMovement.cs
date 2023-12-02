a0362d79c2942225ad0e3f4271c5fdd544af4e91
Victor Kammerer <vko@cin.ufpe.br>
2023-12-02 17:10:57 -0300

    smooth run and jump added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,57 +2,194 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float horizontal;
-    [SerializeField] private float speed = 8f;
-    [SerializeField] private float jumpingPower = 16f;
-    private bool isFacingRight = true;
+    public PlayerData Data;
 
-    private Rigidbody2D rb;
-    new private CapsuleCollider2D collider;
-    [SerializeField] private LayerMask groundLayer;
+    public Rigidbody2D rb { get; private set; }
+
+    public bool IsFacingRight { get; private set; }
+    public bool IsJumping { get; private set; }
+
+    public float LastOnGroundTime { get; private set; }
+    
+    private bool _isJumpCut;
+    private bool _isJumpFalling;
+
+    private Vector2 _moveInput;
+    public float LastPressedJumpTime { get; private set; }
+
+    [SerializeField] private Transform _groundCheckPoint;
+    [SerializeField] private Vector2 _groundCheckSize = new Vector2(0.49f, 0.03f);
+
+    [SerializeField] private LayerMask _groundLayer;
 
     private void Awake() 
     {
         rb = GetComponent<Rigidbody2D>();
-        collider = GetComponent<CapsuleCollider2D>();
     }
 
-    void Update()
+    private void Start()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-
-        if (Input.GetButtonDown("Jump") && IsGrounded())
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
-            
-        }
-
-        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-        }
-
-        Flip();
+        SetGravityScale(Data.gravityScale);
+        IsFacingRight = true;
     }
+
+    private void Update()
+    {
+        LastOnGroundTime -= Time.deltaTime;
+        LastPressedJumpTime -= Time.deltaTime;
+
+        _moveInput.x = Input.GetAxisRaw("Horizontal");
+        _moveInput.y = Input.GetAxisRaw("Vertical");
+
+        if (_moveInput.x != 0)
+            CheckDirectionToFace(_moveInput.x > 0);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            OnJumpInput();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            OnJumpUpInput();
+        }
+
+        if (!IsJumping)
+        {
+            if (Physics2D.OverlapBox(_groundCheckPoint.position, _groundCheckSize, 0, _groundLayer) && !IsJumping)
+            {
+                LastOnGroundTime = Data.coyoteTime;
+            }       
+        }
+
+        if (IsJumping && rb.velocity.y < 0)
+        {
+            IsJumping = false;
+        }
+
+        if (LastOnGroundTime > 0 && !IsJumping)
+        {
+            _isJumpCut = false;
+
+            if(!IsJumping)
+                _isJumpFalling = false;
+        }
+
+        if (CanJump() && LastPressedJumpTime > 0)
+        {
+            IsJumping = true;
+            _isJumpCut = false;
+            _isJumpFalling = false;
+            Jump();
+        }
+
+        if (rb.velocity.y < 0 && _moveInput.y < 0)
+        {
+            SetGravityScale(Data.gravityScale * Data.fastFallGravityMult);
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Max(rb.velocity.y, -Data.maxFastFallSpeed));
+        }
+        else if (_isJumpCut)
+        {
+            SetGravityScale(Data.gravityScale * Data.jumpCutGravityMult);
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Max(rb.velocity.y, -Data.maxFallSpeed));
+        }
+        else if ((IsJumping || _isJumpFalling) && Mathf.Abs(rb.velocity.y) < Data.jumpHangTimeThreshold)
+        {
+            SetGravityScale(Data.gravityScale * Data.jumpHangGravityMult);
+        }
+        else if (rb.velocity.y < 0)
+        {
+            SetGravityScale(Data.gravityScale * Data.fallGravityMult);
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Max(rb.velocity.y, -Data.maxFallSpeed));
+        }
+        else
+        {
+            SetGravityScale(Data.gravityScale);
+        }
+    }   
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        Run(1);
     }
 
-    private bool IsGrounded()
+    public void OnJumpInput()
     {
-        return Physics2D.BoxCast(collider.bounds.center, collider.size, 0f, Vector2.down, .025f, groundLayer);
+        LastPressedJumpTime = Data.jumpInputBufferTime;
     }
 
-    private void Flip()
+    public void OnJumpUpInput()
     {
-        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        if (CanJumpCut())
+            _isJumpCut = true;
+    }
+
+    private void Run(float lerpAmount){
+        float targetSpeed = _moveInput.x * Data.runMaxSpeed;
+        targetSpeed = Mathf.Lerp(rb.velocity.x, targetSpeed, lerpAmount);
+
+        float accelRate;
+        if (LastOnGroundTime > 0)
+            accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? Data.runAccelAmount : Data.runDeccelAmount;
+        else
+            accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? Data.runAccelAmount * Data.accelInAir : Data.runDeccelAmount * Data.deccelInAir;
+
+        if ((IsJumping || _isJumpFalling) && Mathf.Abs(rb.velocity.y) < Data.jumpHangTimeThreshold)
         {
-            isFacingRight = !isFacingRight;
-            Vector3 localScale = transform.localScale;
-            localScale.x *= -1f;
-            transform.localScale = localScale;
+            accelRate *= Data.jumpHangAccelerationMult;
+            targetSpeed *= Data.jumpHangMaxSpeedMult;
         }
+        
+        if(Data.doConserveMomentum && Mathf.Abs(rb.velocity.x) > Mathf.Abs(targetSpeed) && Mathf.Sign(rb.velocity.x) == Mathf.Sign(targetSpeed) && Mathf.Abs(targetSpeed) > 0.01f && LastOnGroundTime < 0)
+        {
+            accelRate = 0; 
+        }
+
+        float speedDif = targetSpeed - rb.velocity.x;
+        float movement = speedDif * accelRate;
+
+        rb.AddForce(movement * Vector2.right, ForceMode2D.Force);
     }
-} 
+
+    private void Turn()
+    {
+        Vector3 scale = transform.localScale; 
+        scale.x *= -1;
+        transform.localScale = scale;
+
+        IsFacingRight = !IsFacingRight;
+    }
+
+    private void Jump()
+    {
+        LastPressedJumpTime = 0;
+        LastOnGroundTime = 0;
+
+        float force = Data.jumpForce;
+        if (rb.velocity.y < 0)
+            force -= rb.velocity.y;
+
+        rb.AddForce(Vector2.up * force, ForceMode2D.Impulse);
+    }
+    
+    public void SetGravityScale(float scale)
+    {
+        rb.gravityScale = scale;
+    }
+
+    private void CheckDirectionToFace(bool isMovingRight)
+    {
+        if (isMovingRight != IsFacingRight)
+            Turn();
+    }
+
+    private bool CanJump()
+    {
+        return LastOnGroundTime > 0 && !IsJumping;
+    }
+
+    private bool CanJumpCut()
+    {
+        return IsJumping && rb.velocity.y > 0;
+    }
+}
